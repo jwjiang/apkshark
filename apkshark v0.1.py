3999790e348a363d00d411d0b10ac1ac94e01bcf
jwjiang <jwjiang@princeton.edu>
2015-06-15 16:53:06 +0100

    Added find_names3 which is more modular - removes the need for the script file.

--- apkshark v0.1.py
+++ apkshark v0.1.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 
 __author__ = 'jjiang'
 
@@ -12,7 +12,17 @@ from subprocess import PIPE, Popen
 
 def find_names(apkname, namefile):
     name = subprocess.check_output('./package_name.sh ' + apkname, shell=True, universal_newlines=True)
-    namefile.write(apkname + ',' + name)
+    namefile.write(''.join([apkname, ',', name]))
+
+# more modular - can insert location of aapt
+def find_names3(apkname, namefile):
+    p = subprocess.Popen('echo `/usr/share/android-sdk/sdk/build-tools/android-4.4W/aapt dump badging ' + apkname +
+                         ' | grep package | awk \'{print $2}\' | sed s/name=//g | sed s/\\\'//g`', shell=True,
+                         stdout=PIPE, universal_newlines=True)
+    name = p.communicate()[0]
+    namefile.write(''.join([apkname, ',', name]))
+
+start_time = time.time()
 
 # make script file executable
 st = os.stat('package_name.sh')
@@ -20,7 +30,10 @@ os.chmod('package_name.sh', st.st_mode | stat.S_IEXEC)
 
 # check for path as argument
 if len(sys.argv) < 2:
-    print("Please specify only the directory of the .apk files")
+    print('Please specify the directory of the .apk files')
+    sys.exit(-1)
+elif len(sys.argv) > 2:
+    print('Please specify only the directory of the .apk files')
     sys.exit(-1)
 else:
     directory = sys.argv[1]
@@ -54,11 +67,13 @@ for apk_name in apk_list:
     if apk_name[len(apk_name)-4:] != '.apk':
         apk_list.remove(apk_name)
     else:
-        find_names(apk_name, package_output)
+        find_names3(apk_name, package_output)
         count += 1
         if size%1 == 0:
             print(str(count) + '/' + str(size) + ' processed.')
 print(str(size-count) + ' non-apk files')
 # save list
 package_output.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
 
